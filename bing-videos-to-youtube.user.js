bc0455d8778b8a2faa6a6e4d0f78422f722bab41
Mohit Raj <agrmohit@outlook.com>
2022-11-28 21:59:17 +0530

    Add bing-videos-to-youtube.user.js

---
+++ bing-videos-to-youtube.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name        Bing Videos to YouTube
+// @namespace   agrmohit
+// @match       https://www.bing.com/videos/*&&view=detail&mid*
+// @match       https://www.bing.com/videos/search?*
+// @grant       none
+// @version     1.0
+// @author      agrmohit
+// @description Redirect youtube videos playing in bing to youtube
+// @license     MIT
+// ==/UserScript==
+
+const selector = ".mmvdp_meta_title_link";
+
+window.addEventListener("load", () => {
+  if (document.querySelector(selector)) {
+    window.location = document.querySelector(selector).href;
+  }
+});

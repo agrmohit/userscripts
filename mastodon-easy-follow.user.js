a69c5314acffbe4d43c6123211c56a088dbd00c4
Mohit Raj <agrmohit@outlook.com>
2022-11-29 00:54:11 +0530

    Attempt to fix script and add mastodon v3 compatibility

--- mastodon-easy-follow.user.js
+++ mastodon-easy-follow.user.js
@@ -2,7 +2,7 @@
 // @name        Mastodon easy follow
 // @namespace   agrmohit
 // @grant       none
-// @version     1.0.0
+// @version     1.1.0
 // @author      agrmohit
 // @description Open the mastodon profile in your own instance by clicking on their @username@instance
 // @downloadURL https://github.com/agrmohit/userscripts/raw/main/mastodon-easy-follow.user.js
@@ -28,15 +28,23 @@
 // @match       https://mstdn.social/*
 // @match       https://toot.community/*
 // @match       https://phpc.social/*
+// @match       https://social.network.europa.eu/*
 // @icon        https://www.google.com/s2/favicons?sz=128&domain=joinmastodon.org
 // @license     MIT
 // ==/UserScript==
 
 const disconnect = VM.observe(document.body, () => {
   // Select element of interest
-  const node = document.querySelector(
+  let node = document.querySelector(
     ".account__header__tabs__name > h1 > small"
   );
+
+  // For compatibility with mastodon v3.x.x
+  if (!node) {
+    node = document.querySelector(
+      ".public-account-header__tabs__name > h1 > small"
+    );
+  }
 
   if (node) {
     const own_instance = "hachyderm.io";
@@ -51,6 +59,3 @@ const disconnect = VM.observe(document.body, () => {
     return true;
   }
 });
-
-// Disconnect the observer explicitly when it's not used any more
-disconnect();

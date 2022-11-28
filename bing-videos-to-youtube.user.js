ad6b27bd7adb9573a485d4960fed64032d9ddf73
Mohit Raj <agrmohit@outlook.com>
2022-11-29 00:30:13 +0530

    Rearrange code

--- bing-videos-to-youtube.user.js
+++ bing-videos-to-youtube.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name        Bing Videos to YouTube
 // @namespace   agrmohit
-// @match       https://www.bing.com/videos/*&&view=detail&mid*
-// @match       https://www.bing.com/videos/search?*
 // @grant       none
-// @version     1.0.1
+// @version     1.1.0
 // @author      agrmohit
 // @description Redirect youtube videos playing in bing to youtube
 // @downloadURL https://github.com/agrmohit/userscripts/raw/main/bing-videos-to-youtube.user.js
+// @match       https://www.bing.com/videos/*&&view=detail&mid*
+// @match       https://www.bing.com/videos/search?*
 // @license     MIT
 // ==/UserScript==
 
